d3b57f7f5a67915dc70610e8d887578d4f90746f
Luis Antonio Peña Murillo <apmurillo99@hotmail.com>
2025-12-11 23:37:34 -0600

    feat: Update files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,9 @@
+import { PokemonListPageComponentComponent } from '../app/pokemon/components/pokemon-list-page-component/pokemon-list-page-component.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-/*
-    { path: '', component: PokemonListComponent },
-  { path: 'new', component: PokemonCreateComponent },
-  { path: ':id/edit', component: PokemonEditComponent },
-  { path: ':id', component: PokemonDetailComponent }
-   */
+  { path: '', component: PokemonListPageComponentComponent },
+  //{ path: 'new', component: PokemonCreateComponent },
+  //{ path: ':id/edit', component: PokemonEditComponent },
+  //{ path: ':id', component: PokemonDetailComponent },
 ];
-
-/*/pokemons → Lista
-
-/pokemons/new → Crear
-
-/pokemons/:id/edit → Editar
-
-/pokemons/:id → Detalle (opcional)
-*/
