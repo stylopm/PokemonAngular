f82eee713f38ee5db823f6cb364f065bd4b939a7
Luis Antonio Peña Murillo <apmurillo99@hotmail.com>
2026-01-19 23:09:57 -0600

    feat: Update table

--- src/app/pokemon/components/pokemon-table/pokemon-table.component.ts
+++ src/app/pokemon/components/pokemon-table/pokemon-table.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { Pokemon } from '@models/pokemon.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-table',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './pokemon-table.component.html',
   styleUrl: './pokemon-table.component.css'
 })
 export class PokemonTableComponent {
-
+  @Input() pokemons: Pokemon[] = [];
 }
--- src/app/pokemon/pages/pokemon-list-page-component/pokemon-list-page-component.component.ts
+++ src/app/pokemon/pages/pokemon-list-page-component/pokemon-list-page-component.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Pokemon } from '@models/pokemon.model';
 import { PokemonService } from '@services/pokemon.service';
+import { PokemonTableComponent } from '@components/pokemon-table/pokemon-table.component';
 import { RouterLink } from '@angular/router';
 import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-list-page-component',
-  imports: [CommonModule, RouterLink ],
+  imports: [CommonModule, RouterLink, PokemonTableComponent],
   templateUrl: './pokemon-list-page-component.component.html',
   styleUrl: './pokemon-list-page-component.component.css',
 })
@@ -26,7 +27,7 @@ export class PokemonListPageComponentComponent {
       .pipe(
         finalize(() => {
           this.loading = false;
-        })
+        }),
       )
       .subscribe({
         next: (pokemons) => {
