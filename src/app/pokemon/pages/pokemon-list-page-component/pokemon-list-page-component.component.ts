85010c3ef287f0f3833310e9708b23f253e1673b
Luis Antonio Peña Murillo <apmurillo99@hotmail.com>
2026-01-19 20:05:27 -0600

    feat: Update files

--- src/app/pokemon/pages/pokemon-list-page-component/pokemon-list-page-component.component.ts
+++ src/app/pokemon/pages/pokemon-list-page-component/pokemon-list-page-component.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Pokemon } from '@models/pokemon.model';
 import { PokemonService } from '@services/pokemon.service';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-list-page-component',
@@ -11,17 +12,30 @@ import { PokemonService } from '@services/pokemon.service';
 })
 export class PokemonListPageComponentComponent {
   pokemons: Pokemon[] = [];
-
+  loading = true;
   constructor(private pokemonService: PokemonService) {}
   ngOnInit(): void {
     this.loadPokemons();
   }
 
-
   loadPokemons(): void {
-    this.pokemonService.getPokemons().subscribe((pokemons) => {
-      this.pokemons = pokemons;
-    });
+    this.loading = true;
+    this.pokemonService
+      .getPokemons()
+      .pipe(
+        finalize(() => {
+          this.loading = false;
+        })
+      )
+      .subscribe({
+        next: (pokemons) => {
+          this.pokemons = pokemons;
+          this.loading = false;
+        },
+        error: (error) => {
+          console.error(error);
+          this.loading = false;
+        },
+      });
   }
-
 }
