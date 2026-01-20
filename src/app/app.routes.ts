c569966e1c579905d39ca6a5c9ffc4d3ffc666a7
Luis Antonio Peña Murillo <apmurillo99@hotmail.com>
2026-01-19 21:25:29 -0600

    feat: Update create and edit pokemons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,13 @@
 import { CreatePokemonComponent } from '@pages/create-pokemon/create-pokemon.component';
 import { EditPokemonComponent } from '@pages/edit-pokemon/edit-pokemon.component';
-import { PokemonDetailPageComponentComponent } from '@components/pokemon-detail-page-component/pokemon-detail-page-component.component';
 import { PokemonListPageComponentComponent } from '@pages/pokemon-list-page-component/pokemon-list-page-component.component';
 import { Routes } from '@angular/router';
 import { SessionNotFoundComponent } from '@pages/session-not-found/session-not-found.component';
 
 export const routes: Routes = [
   { path: '', component: PokemonListPageComponentComponent },
-  { path: 'pokemon/:id', component: PokemonDetailPageComponentComponent },
-  { path: 'pokemon/new', component: CreatePokemonComponent },
-  { path: 'pokemon/:id/edit', component: EditPokemonComponent },
+  { path: 'pokemon/new', component: CreatePokemonComponent },  
+  { path: 'pokemon/edit', component: EditPokemonComponent },
   { path: 'session-not-found', component: SessionNotFoundComponent },
   { path: '**', redirectTo: 'session-not-found' },
 ];
