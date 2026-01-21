5e22e6e0ea213a4ff253ae74b233acf50602d456
Luis Antonio Peña Murillo <apmurillo99@hotmail.com>
2026-01-20 21:33:51 -0600

    feat: Update pokemon form

--- src/app/pokemon/components/pokemon-form/pokemon-form.component.ts
+++ src/app/pokemon/components/pokemon-form/pokemon-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -18,7 +18,7 @@ import { RouterLink } from '@angular/router';
   templateUrl: './pokemon-form.component.html',
   styleUrl: './pokemon-form.component.less',
 })
-export class PokemonFormComponent {
+export class PokemonFormComponent implements OnChanges {
   @Input() pokemon: Pokemon | null = null;
   form!: FormGroup;
 
@@ -28,6 +28,12 @@ export class PokemonFormComponent {
     private router: Router,
   ) {
     this.createForm();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['pokemon']?.currentValue) {
+      this.form.patchValue(changes['pokemon'].currentValue);
+    }
   }
 
   private createForm() {
@@ -41,20 +47,28 @@ export class PokemonFormComponent {
       ],
       hp: [null, [Validators.required, Validators.min(1), Validators.max(999)]],
     });
-
-    if (this.pokemon) {
-      this.form.patchValue(this.pokemon);
-    }
   }
   save() {
     if (this.form.invalid) {
       this.form.markAllAsTouched();
       return;
     }
-    const pokemon: Pokemon = this.form.value;
-    this.ps.createPokemon(pokemon).subscribe((pokemonNew) => {
-      alert(`Pokemon new ${pokemonNew.id} ${pokemonNew.name}`);
-      this.router.navigate(['/']);
-    });
+
+    const pokemonValue: Pokemon = this.form.value;
+
+    if (this.pokemon) {
+      const id = this.pokemon?.id ?? pokemonValue.id;
+      if (id != null) {
+        this.ps
+          .updatePokemon(id, { ...pokemonValue, id })
+          .subscribe((pokemonNew) => {
+            this.router.navigate(['/']);
+          });
+      }
+    } else {
+      this.ps.createPokemon(pokemonValue).subscribe((pokemonNew) => {
+        this.router.navigate(['/']);
+      });
+    }
   }
 }
