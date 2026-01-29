1258fac5f308bf193a85c9637a69cda3a9227a46
Luis Antonio Peña Murillo <apmurillo99@hotmail.com>
2026-01-28 20:53:16 -0600

    feat: Update files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
-import { CreatePokemonComponent } from '@pages/create-pokemon/create-pokemon.component';
-import { EditPokemonComponent } from '@pages/edit-pokemon/edit-pokemon.component';
-import { PokemonListPageComponentComponent } from '@pages/pokemon-list-page-component/pokemon-list-page-component.component';
+import { EditPokemonComponent } from '@pages/pokemon-edit-page/pokemon-edit-page';
+import { PokemonCreateComponent } from '@pages/pokemon-create-page/pokemon-create-page';
+import { PokemonListPageComponentComponent } from '@pages/pokemon-list-page/pokemon-list-page.component';
 import { Routes } from '@angular/router';
 import { SessionNotFoundComponent } from '@pages/session-not-found/session-not-found.component';
 
 export const routes: Routes = [
   { path: '', component: PokemonListPageComponentComponent },
-  { path: 'pokemon/new', component: CreatePokemonComponent },  
+  { path: 'pokemon/new', component: PokemonCreateComponent },  
   { path: 'pokemon/edit/:id', component: EditPokemonComponent },
   { path: 'session-not-found', component: SessionNotFoundComponent },
   { path: '**', redirectTo: 'session-not-found' },
